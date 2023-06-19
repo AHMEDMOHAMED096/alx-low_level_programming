a032ffc41557f7f91a930576b5ebde62740725cd
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-06-19 09:17:31 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charachter %c to stdout
+ * %c the charachter to be printed
+ *
+ * Always return on success 1.
+ */
+int _putchar (char c);
+{
+
+	return (write(1, &c, 1));
+}
