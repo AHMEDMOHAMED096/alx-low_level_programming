80d93a83e2a28b809387635762f6d352ea5b598f
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-07-30 16:51:45 -0700

    debug

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * A function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: Return always 0 on success
+ *
+ */
 
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	_putchar(ch);
-	
+	_putchar('\n');
 	return (0);
 
 }
