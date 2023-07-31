cb45b5565aa6d79e143935e66c5d0787571b455f
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-07-30 17:05:31 -0700

    fixed code style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - prints all the alphabets in lowercase
  *
- * A function that prints the alphabet, in lowercase, followed by a new line.
  * Return: Return always 0 on success
- *
  */
 
 void print_alphabet(void)
