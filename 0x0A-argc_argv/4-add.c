2a079444cab2d6364721bd45401116fcfe6967aa
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-05 15:30:00 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point to the program
+ * add: adds positive numbers.
+ * @argc: the number of command line arguments
+ * @argv: the command line arguments entered
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, add = 0;
+	char *j;
+
+	for (i = 1; i < argc; i++)
+	{
+	j = argv[i];
+	if (argc == 1)
+	{
+	printf("0\n");
+	}
+
+	else if (*j < '0' || *j > '9')
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	else
+	add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
