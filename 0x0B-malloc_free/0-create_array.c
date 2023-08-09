0615f7bc7a7383a77f39cc94f64ac9de1a7763d5
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-09 18:09:32 +0300

    debug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-    unsigned int n;
-    create_array = (char *)malloc(n * sizeof(char));
-    if (create_array == 0)
-    {
-    printf("failed to allocate memory\n");
-    exit(0);
-    }
-    free(create_array);
+unsigned int n;
+
+create_array = (char *)malloc(n * sizeof(char));
+if (create_array == 0)
+{
+exit(0);
+}
+free(create_array);
 }
 
 
