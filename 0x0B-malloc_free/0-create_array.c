f3ec438ad5ea846c75ae009520df389c503db031
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-09 18:15:48 +0300

    debug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /*
-* create_array - A pointer creates an array of chars 
+* create_array - A function pointer creates an array of chars
 * @buffer: the address of memory to print
 * @size: the size of the memory to print
 * Return: return Null if it fails
