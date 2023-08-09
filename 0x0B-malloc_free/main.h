fefb7de7fd5f65248c8b603b6f4c513bc42c5629
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-09 19:20:04 +0300

    debug

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <stdio.h>
 
+void simple_print_buffer(char *buffer, unsigned int size);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
