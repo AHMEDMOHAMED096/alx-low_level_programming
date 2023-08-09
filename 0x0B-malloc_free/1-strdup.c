0455df5c1bd5189aca5743e1f0ebb6e272279a32
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-10 00:57:02 +0300

    debug

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 /**
 * _strdup - returns a pointer to a newly allocated space in memory,
 * which contains a copy of the string given as a parameter.
-*
+* @str: the string to be returned
 * Return: returns a pointer to a new string or Null if str = null
 */
 char *_strdup(char *str)
