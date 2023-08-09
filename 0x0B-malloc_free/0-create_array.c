33e063cf864178896c6d1f912be18326fed9084f
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-09 18:06:50 +0300

    debug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,25 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/*
+* create_array: creates an array of chars, and initializes it with a specific char.
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+* Return: return Null if it fails
+*/
+
+char *create_array(unsigned int size, char c)
+{
+    unsigned int n;
+    create_array = (char *)malloc(n * sizeof(char));
+    if (create_array == 0)
+    {
+        printf("failed to allocate memory\n");
+        exit(0);
+    }
+    free(create_array);
+}
+
+
+
