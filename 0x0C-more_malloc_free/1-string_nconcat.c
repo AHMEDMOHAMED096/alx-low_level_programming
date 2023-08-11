a1243ea9328addd26822bc934450c81d6afe8518
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-11 16:53:38 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings.
+ * @n: the size of memory to print.
+ *
+ * return: return NULL on fail.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+
+
+
+}
