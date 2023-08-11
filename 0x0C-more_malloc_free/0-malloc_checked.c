a54ba6e832d8428de5ecb5997d191e45d20c248f
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-12 02:33:42 +0300

    debug

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* malloc_checked - allocates memory using malloc.
-* @b: the size of memory to print.
-* Return: Returns a pointer to the allocated memory
+*malloc_checked - allocates memory using malloc.
+*@b: the size of memory to print.
+*Return: Returns a pointer to the allocated memory
 */
 
 void *malloc_checked(unsigned int b)
