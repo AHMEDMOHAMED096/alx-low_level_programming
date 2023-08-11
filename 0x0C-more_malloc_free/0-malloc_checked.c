f38245206fa28da1bba4277003c18869ddfa6672
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-12 00:14:21 +0300

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* malloc_checked - allocates memory using malloc.
+*
+* Return: Returns a pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+    void *fun = (void*)malloc(size);
+    if (fun == NULL)
+    return (98);
+}
