e9a7a43f3003f530817098039135392f9129af2d
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-12 10:17:28 -0700

    debug

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include "main.h"
-
 /**
 * malloc_checked - allocates memory using malloc.
 * @b: the size of memory to print.
@@ -13,23 +11,7 @@ void *malloc_checked(unsigned int b)
 
 	if (fun == NULL)
 	exit(98);
+	else
 	return (fun);
 }
-=======
-#include "main.h"
 
-/**
-* malloc_checked - allocates memory using malloc.
-* @b: the size of memory to print.
-* Return: Returns a pointer to the allocated memory
-*/
-
-void *malloc_checked(unsigned int b)
-{
-	void *fun = malloc(b);
-
-	if (fun == NULL)
-	exit(98);
-	return (fun);
-}
->>>>>>> 42a486cb3fe5334ad08ea1acacf72aec9b4933ec
