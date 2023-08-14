df11b564ee18a37cfb42e9b8873a24848a8d9fe4
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-14 07:10:53 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - a structure of data
  * @name: first member
@@ -13,10 +14,7 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-} dog;
-
-#include <stdio.h>
-#include <stdlib.h>
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
