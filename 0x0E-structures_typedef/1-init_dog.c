6c0f69491e3493f71aa1a3f83047646225332563
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-14 07:41:19 -0700

    debug

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d = @dog;
-	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	}
+	struct dog *ptr;
+	ptr = malloc(sizeof(struct dog));
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
 }
