7c34f561b1d492727112bee36ec15b0779118088
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-14 12:02:03 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,15 +3,11 @@
  * print_dog - prints a struct dog
  * struct dog - a structure of dog's data
  * @d: a pointer to struct dog
- * @name: first variable
- * @age: second variable
- * @owner: third variable
- * Return: return nothing
  */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	return;
 	else if (d->name == NULL || d->owner == NULL)
 	{
