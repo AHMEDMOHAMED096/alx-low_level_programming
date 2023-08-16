9caf09b07aedc4347b2a2f7206afb38b47a43823
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-16 04:18:33 +0300

    debug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * dog_t - Typedef for struct dog
@@ -8,14 +9,33 @@
  * Return: return NULL if function fails
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *dog = malloc(sizeof(new_dog);
 
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	dog = new_dog;
-	if (dog == NULL)
-	return (0);
+dog_t *new_dog(char *name, float age, char *owner) 
+{
+    dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+    if (newDog == NULL) 
+	{
+        return NULL; 
+    }
+    
+    newDog->name = (char *)malloc(strlen(name) + 1);
+    if (newDog->name == NULL) 
+	{
+        free(newDog);
+        return NULL; 
+    }
+    strcpy(newDog->name, name);
+    
+    newDog->owner = (char *)malloc(strlen(owner) + 1);
+    if (newDog->owner == NULL) 
+	{
+        free(newDog->name);
+        free(newDog);
+        return NULL;
+    }
+    strcpy(newDog->owner, owner);
+    
+    newDog->age = age;
+    
+    return newDog;
 }
