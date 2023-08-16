841d407cd42828fc6217b84a2c4817a7ccc4395e
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-16 04:28:55 +0300

    debug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <string.h>
 /**
  * new_dog - creates a new dog
  * dog_t - Typedef for struct dog
@@ -18,24 +17,4 @@ dog_t *new_dog(char *name, float age, char *owner)
         return (0);
     }
     
-    newDog->name = malloc(strlen(name) + 1);
-    if (newDog->name == NULL)
-	{
-        free(newDog);
-        return (0);
-    }
-    strcpy(newDog->name, name);
-
-    newDog->owner = malloc(strlen(owner) + 1);
-    if (newDog->owner == NULL)
-	{
-        free(newDog->name);
-        free(newDog);
-        return (0);
-    }
-    strcpy(newDog->owner, owner);
-    
-    newDog->age = age;
-    
-    return newDog;
 }
