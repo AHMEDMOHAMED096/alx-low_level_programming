cf6b6835168239f4cb72840eeabe7d38b9ff7a1f
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2023-08-17 16:24:52 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(args, n);
-	while (n != 0)
+	while (n--)
 	result += va_arg(args, int);
 	va_end(args);
 	return (result);
