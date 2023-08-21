e46aa9109ccb654a939b7498f2c02846d569002c
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-21 20:41:22 +0300

    debug

--- test/_printf.c
+++ test/_printf.c
@@ -1,13 +1,7 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#define BUFFER_SIZE 1024
-#include <limits.h>
 
-int print_char(int c)
+
+int _putchar(char c)
 {
     return write(1, &c, 1);
 }
@@ -17,7 +11,7 @@ int print_str(char *str)
     int count = 0;
     while (*str != '\0')
     {
-    print_char((int)*str);
+    _putchar((int)*str);
     ++count;
     ++str;
     }
@@ -28,14 +22,14 @@ int print_str(char *str)
 int print_digit(long n, int base)
 {
     int count = 0;
-	char symbols[] = "0123456789ABCDEF";
+	char symbols[] = "0123456789abcdef";
     if (n < 0)
     {
         write(1, "-", 1);
         return print_digit(n * -1, base) + 1;
     }
     else if (n < base)
-    return print_char(symbols[n]) + 1;
+    return _putchar(symbols[n]) + 1;
     else
     {
         count = print_digit(n / base , base);
@@ -51,7 +45,7 @@ int print_format(char spec, va_list ap)
     int count = 0;
     
     if (spec == 'c')
-    count += print_char(va_arg(ap, int));
+    count += _putchar(va_arg(ap, int));
     else if (spec == 's')
     count += print_str(va_arg(ap, char *));
     else if (spec == 'i' || spec == 'd')
@@ -65,11 +59,11 @@ int print_format(char spec, va_list ap)
     else if (spec == 'p')
     count += print_digit((long)(va_arg(ap, void *)), 16);
     else if (spec == '%')
-    count += print_char('%');
+    count += _putchar('%');
     else if (spec == 'r')
     {
-    count += print_char('%');
-    count += print_char('r');
+    count += _putchar('%');
+    count += _putchar('r');
     }
     else 
     count += write(1, &spec, 1);
