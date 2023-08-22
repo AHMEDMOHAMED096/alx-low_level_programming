e7e134e3a642316cb13a2f3875c40b737173f4ea
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-23 00:56:47 +0300

    debug

--- test/_printf.c
+++ test/_printf.c
@@ -65,7 +65,6 @@ int _printf(const char *format, ...)
                 }
                 else
                 {
-                    count += write(1, &format[i], 1);
                     count += write(1, &format[i + 1], 1);
                 }
             }
