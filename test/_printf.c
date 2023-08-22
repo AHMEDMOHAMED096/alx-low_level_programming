ca35957e2df1da7dcf04dde5d631275c80759747
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-23 00:50:09 +0300

    debug

--- test/_printf.c
+++ test/_printf.c
@@ -50,9 +50,9 @@ int _printf(const char *format, ...)
             {
                 count += write(1, &format[i], 1);
                 count += write(1, &format[i + 1], 1);
+            else
+            count += print_format((format[++i]), ap);
             }
-        else
-        count += print_format((format[++i]), ap);
         }
         else
         count += write(1, format + i, 1);
