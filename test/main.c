f07aced3ea3ca2bb523ff990ff3e776c969125a2
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-22 21:48:41 +0300

    debug

--- test/main.c
+++ test/main.c
@@ -11,8 +11,8 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
-    int num = 1027;
-    int num2 = -1027;
+    /*int num = 1027;
+    int num2 = -1027;*/
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
