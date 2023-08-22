e1c3885e266df8067d18e2447ba245e06c01727f
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-22 23:32:00 +0300

    debug

--- test/_printf.c
+++ test/_printf.c
@@ -32,6 +32,8 @@ int print_format(char spec, va_list ap)
     count += _putchar('%');
     count += _putchar('r');
     }
+    else if (spec == ' ');
+    return (-1);
     else 
     count += write(1, &spec, 1);
     return count;
