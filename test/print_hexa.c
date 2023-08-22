fd9710e73230b8e9a79503bd0b381fb357987cb5
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-23 02:19:27 +0300

    modify style and documentation

--- test/print_hexa.c
+++ test/print_hexa.c
@@ -2,12 +2,13 @@
 /**
 * print_hexa - prints hexadecimal numbers to stdout
 * according to its casespeifier.
+* @casespecifier: to differentiate between uppercase and lowercase
 * @n: given number by variadic arguments list
 * @base: the base of given number
 * Return: return number of printed characters
 */
 
-int print_hexa(long n, int base, char caseSpecifier)
+int print_hexa(long n, int base, char casespecifier)
 {
 	int count = 0;
 	char symbolsLower[] = "0123456789abcdef";
@@ -17,13 +18,13 @@ int print_hexa(long n, int base, char caseSpecifier)
 	if (n < 0)
 	{
 		write(1, "-", 1);
-		return (print_hexa(n * -1, base, caseSpecifier) + 1);
+		return (print_hexa(n * -1, base, casespecifier) + 1);
 	}
 	else if (n < base)
 	return (_putchar(symbols[n]) + 1);
 	else
 	{
-	count = print_hexa(n / base, base, caseSpecifier);
-	return (count + print_hexa(n % base, base, caseSpecifier));
+	count = print_hexa(n / base, base, casespecifier);
+	return (count + print_hexa(n % base, base, casespecifier));
 	}
 }
