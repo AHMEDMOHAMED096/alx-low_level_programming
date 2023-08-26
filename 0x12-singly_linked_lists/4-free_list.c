62768b1f58207cc7782662d69d4eb11d88a4af99
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-26 18:01:28 +0300

    free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,16 +2,18 @@
 
 /**
 * free_list - that frees a list_t list.
+* @head: pointer to struct list_t
 */
 
 void free_list(list_t *head)
 {
-    list_t *temp;
-    while (head != NULL)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	list_t *temp;
+	
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 
 }
