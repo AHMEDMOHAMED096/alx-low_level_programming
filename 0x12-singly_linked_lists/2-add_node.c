b92d886be8bd4fa95de0d223218ec9e0130002b7
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-26 16:45:26 +0300

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,13 +2,15 @@
 
 /**
 * add_node - adds a new node at the beginning of a list_t list.
-
-
+* @head: a pointer to pointer of head
+* @str: string to be printed
+* Return: return the address of the new element
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
+
 	if (ptr == NULL)
 	return (0);
 	ptr->str = strdup(str);
