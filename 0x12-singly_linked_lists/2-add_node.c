bc84ceb2fc943731c13a8d2c5dc2daaa34019f44
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-26 17:02:35 +0300

    add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (ptr == NULL)
 	return (0);
-	ptr->str = strndup(str, sizeof(h->len));
+	ptr->str = strndup(str, sizeof(str));
 	ptr->next = *head;
 	*head = ptr;
 	return (*head);
