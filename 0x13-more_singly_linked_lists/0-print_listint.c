10a7ceba20b2f404d5677d5e8dd16d978fba060e
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-28 20:14:55 +0300

    print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listlen - prints all the elements of a listint_t list
+* @h: pointer to struct list_t
+* Return: return size_t
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
