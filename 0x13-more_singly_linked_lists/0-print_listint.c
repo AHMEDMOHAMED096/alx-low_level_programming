864697e1318aec4fb1f13e3395a7e722dce2e013
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-28 20:29:02 +0300

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,14 @@ size_t print_listint(const listint_t *h)
 
 		while (h != NULL)
 		{
+			if (h->n == NULL)
+			return (0);
+			else
+			{
 			printf("%d\n", h->n);
 			h = h->next;
 			count++;
+			}
 		}
 	return (count);
 }
