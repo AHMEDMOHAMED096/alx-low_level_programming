e3d1f4a212ad157bc9c448dce71097ed80a2007d
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-30 00:18:05 +0300

    6-pop_listint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,11 +10,13 @@
 int pop_listint(listint_t **head)
 {
 	if (*head == NULL)
+	{
 	return (0);
-	int data = (*head)->n;
+	}
+	int n = (*head)->n;
 	listint_t *temp = *head;
 
 	*head = (*head)->next;
 	free(temp);
-	return (data);
+	return (n);
 }
