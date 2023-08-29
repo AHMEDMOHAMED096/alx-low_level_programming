81f6cde72b6b4b088ecdaad6f3cb825fb8f5fcc2
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-29 15:45:50 +0300

    4-free_listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,11 +7,10 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *temp = head;
 
 	while (temp != NULL)
 	{
-		temp = head;
 		temp = temp->next;
 		free(head);
 		head = temp;
