1d33a997f6c078e9ebf5c071f91fddb1ef8db052
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-30 13:07:28 +0300

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr = head;
 	listint_t *current;
 	current->n = n;
+	
 	current->next = NULL;
 	unsigned int position = 0;
 
