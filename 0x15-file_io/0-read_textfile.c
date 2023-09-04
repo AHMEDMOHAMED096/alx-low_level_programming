f5627f31ea869bf9fcde31c7e2ab051cab5cc42a
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-05 02:05:24 +0300

    0-read_textfile main

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,31 @@
 #include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: file to be read 
+* @letters: the number of letters it should read and print
+* Return: amount of bytes
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int *fd = open(filename, O_RDWR);
+	char *s = malloc(letters);
+	char *buffer = s;
+	int count = 0;
+
+	if (fd == NULL || filename == NULL)
+		return (0);
+
+	read(fd, buffer, sizeof(filename));
+
+	for (int i = 0; filename[i] != '\0'; i++)
+	{
+		buffer[count] = filename[i];
+		buffer[++count]; 
+	}
+	write(1, filename, sizeof(filename));
+	free(s);
+	close(fd);
+	return (count);
+}
