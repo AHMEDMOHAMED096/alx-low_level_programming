7fb49ec5deebf62089944097440c28d895232fed
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-06 00:13:38 +0300

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,21 +11,25 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	int write_text;
 
-	if (filename != NULL)
-	{
-		fd = open(filename, O_WRONLY | O_APPEND);
-	else
+	if (!filename)
 		return (-1);
-	}
 
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
+	
 	if (text_content != NULL)
 	{
-		write(fd, text_content, strlen(text_content));
-	else
+	write_text = write(fd, text_content, strlen(text_content));
+		if (write_text == -1)
+		{
+		close(fd);
 		return (-1);
+		}
+	else
+	return (1);
 	}
 	close(fd);
 	return (1);
