ed30b1a8b441f9031264a3471530532fd225547a
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-05 16:23:40 +0300

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -43,7 +43,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			return (0);
 			close(fd);
 		}
-		if (read_bytes < buffer)
+		if (read_bytes < *buffer)
 		{
 			free(buffer);
 			return (0);
