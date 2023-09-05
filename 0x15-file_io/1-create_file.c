37162a56d50572289fbb94fd73d8fbfe9074e747
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-05 23:33:14 +0300

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL)
+	if (!text_content)
 	{
 	write_text = write(fd, text_content, strlen(text_content));
 		if (write_text == -1)
