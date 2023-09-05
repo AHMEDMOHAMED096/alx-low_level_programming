e8036b6db1e4815aa71a4cf1b571fc60a3b70ea9
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-05 03:45:06 +0300

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	read_bytes = read(fd, buffer, letters);
-	while (read_bytes != buffer)
+	while (read_bytes != '\0')
 	{
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 		if (write_bytes == -1 || write_bytes != read_bytes)
