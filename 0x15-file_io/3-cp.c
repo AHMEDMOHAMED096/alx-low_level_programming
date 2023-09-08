e1473c05f7b48ec9fddd5c6db85115890c681b0e
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-09 00:40:30 +0300

    debug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,3 +42,21 @@ void cp_file(const char *file_from, const char *file_to)
 		exit(100);
 	}
 }
+
+/**
+ * main - check the code
+ * @ac: the number of command line arguments
+ * @av: the command line arguments to be passed
+ * Return: Always 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp_file(av[1], av[2]);
+	return (0);
+}
