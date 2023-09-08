ca33822c5c3fb2e5cdc45f51ef339a0a841d5c03
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-09-09 01:00:22 +0300

    debug

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,14 +9,14 @@ void cp_file(const char *file_from, const char *file_to);
  * Return: Always 0
  */
 
-int main(int ac, char **av)
+int main(int argc, char **argv[])
 {
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	cp_file(av[1], av[2]);
+	cp_file(argv[1], argv[2]);
 	return (0);
 }
 
@@ -28,36 +28,34 @@ int main(int ac, char **av)
 
 void cp_file(const char *file_from, const char *file_to)
 {
-	int fd;
-	int fd2;
+	int from;
+	int to;
 	int close_file;
 	ssize_t read_bytes = 0;
 	ssize_t write_bytes = 0;
 	char *buffer = malloc(BUFFER_SIZE);
 
-	fd2 = open(file_from, O_RDONLY, S_IRUSR | S_IWUSR
-												| S_IRGRP | S_IWGRP | S_IROTH);
-	read_bytes = read(fd2, buffer, BUFFER_SIZE);
+	from = open(argv[1], O_RDONLY);
+	read_bytes = read(from, buffer, BUFFER_SIZE);
 
 	if (file_from == NULL || buffer == NULL || read_bytes == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	fd = open("file_to", O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR
-												| S_IRGRP | S_IWGRP | S_IROTH);
-	write_bytes = write(fd, buffer, read_bytes);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	write_bytes = write(to, buffer, read_bytes);
 	if (file_to == NULL || write_bytes == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	free(buffer);
 	close_file = close(fd);
 	if (close_file == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
