589ffbd576f3022c67394ccb02c305e7b7c07031
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-10-14 00:54:46 +0300

    7-insert_dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,10 +20,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = NULL;
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
@@ -32,11 +35,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
 			temp->next = new_node;
 			return (new_node);
 		}
 		else
-		temp = temp->next;
+			temp = temp->next;
 	}
+	free(new_node);
 	return (NULL);
 }
