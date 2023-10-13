3721290ed2a2a496dec08167afbfcebfcc0aa956
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-10-13 21:56:05 +0300

    3-add_dnodeint_end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,8 +13,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
-	ptr->n = n;
-	ptr->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 	if (*head == NULL)
 		*head = new_node;
 	else
@@ -26,6 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			ptr = ptr->next;
 		}
 		ptr->next = new_node;
+		new_node->prev = ptr;
 	}
 	return (*head);
 }
