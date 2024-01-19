10aa715683a83c0a2e7c449f0fa3d717a02fc2fe
AHMEDMOHAMED096 <ahmed.mohamed91196@gmail.com>
2024-01-19 09:15:03 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,11 @@
 int print_last_digit(int n)
 {
 	int lastDigit;
-
+	
 	lastDigit = n % 10;
+	if (lastDigit < 0)
+		lastDigit = -lastDigit;
+
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 	_putchar('\n');
