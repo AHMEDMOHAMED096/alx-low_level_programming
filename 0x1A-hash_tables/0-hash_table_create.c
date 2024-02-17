6d6999928fe7872ea56ed5eca641f916a6c8d3ad
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-02-17 11:29:52 -0800

    created repo & solved first task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - A function to create a hash table.
+* @size: The size of array of pointers to linked lists first node.
+* Return: A pointer to newly created hash table.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	int i = 0;
+	hash_table_t *hash_table_ptr = NULL;
+	hash_node_t *head = NULL;
+
+	hash_table_ptr = malloc(sizeof(hash_table_t));
+	if (hash_table_ptr == NULL)
+	{
+		free(hash_table_ptr);
+		return (NULL);
+	}
+
+	hash_table_ptr->array = malloc(size * sizeof(head));
+	if (hash_table_ptr->array == NULL)
+	{
+		free(hash_table_ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table_ptr->array[i] = NULL;
+	}
+
+	hash_table_ptr->size = size;
+
+	return (hash_table_ptr);
+}
