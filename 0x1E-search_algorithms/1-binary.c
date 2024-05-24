f65ea98b469d28e6f404462ec731e5ae0d49f281
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-05-25 00:10:38 +0300

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,41 @@
 #include "search_algos.h"
 
 /**
+ * binary_search_recurssive - a binary search function using recurssion
+ * @array: The array to be searched in
+ * @lt: The left portion of array
+ * @rt: The right portion of array
+ * @value: The value to be searched for
+ * Return: The index of value
+ */
+
+int binary_search_recurssive(int *array, size_t lt, size_t rt, int value)
+{
+	size_t i, mid;
+
+	if (lt <= rt)
+	{
+		mid = (rt + lt) / 2;
+		printf("Searching in array: ");
+		for (i = lt; i <= rt; i++)
+		{
+			printf("%d", array[i]);
+			if (i != rt)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			return (binary_search_recurssive(array, mid + 1, rt, value));
+		else
+			return (binary_search_recurssive(array, lt, mid - 1, value));
+	}
+	return (-1);
+}
+
+/**
  * binary_search - function that searches for a value in a sorted array of ints
  * @array: The array to be searched in
  * @size: The size of array
@@ -10,34 +45,13 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int pivot;
-	size_t i;
+	size_t rt, lt;
 
-	if (size == 0 || array == NULL)
+	rt = size - 1;
+	lt = 0;
+
+	if (array == NULL)
 		return (-1);
 
-	pivot = size / 2;
-
-	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
-	{
-		printf("%d", array[i]);
-		if (i < size - 1)
-			printf(", ");
-	}
-	printf("\n");
-	if (array[pivot] == value)
-		return (pivot);
-	else if (array[pivot] < value)
-	{
-		int result;
-
-		result = (binary_search(array + pivot + 1, size - pivot - 1, value));
-		if (result == -1)
-			return (-1);
-		else
-			return (pivot + result + 1);
-	}
-	else
-		return (binary_search(array, pivot, value));
+	return (binary_search_recurssive(array, lt, rt, value));
 }
