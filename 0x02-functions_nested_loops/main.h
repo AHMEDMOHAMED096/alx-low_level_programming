39c68979c2191a01f41a1e9d554ca46c6d7b726b
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-07-22 18:07:23 +0300

    added function prototype to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <ctype.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,6 +16,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 
 #endif
